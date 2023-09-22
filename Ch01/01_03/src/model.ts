072034d894aa53863f7ba8c5479b786ce65812ab
Joseph Smith <joseph.smith@evident.global>
2023-09-22 15:41:22 +0100

    complete 01_03

--- Ch01/01_03/src/model.ts
+++ Ch01/01_03/src/model.ts
@@ -2,21 +2,20 @@ let displayName: string = "Jess's standing desk";
 let inventoryType: string = "furniture";
 let trackingNumber: string = "FD123455";
 let createDate: Date = new Date();
-let originalCost: number = 425;
+let originalCost: any = 425;
+originalCost = "A LOT!";
 
 function getInventoryItem(trackingNumber: string): {
-    displayName: string;
-    inventoryType: string;    
-    trackingNumber: string;    
-    createDate: Date;    
-    originalCost: number;    
+  displayName: string;
+  inventoryType: string;
+  trackingNumber: string;
+  createDate: Date;
+  originalCost: number;
 } {
-    return null;
+  return null;
 }
 
-function saveInventoryItem(item) {
-
-}
+function saveInventoryItem(item) {}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
